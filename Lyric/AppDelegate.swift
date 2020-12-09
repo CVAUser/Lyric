3985392e28f6a93c2a7fd784e4f259d062b92815
Slava <cherkasyuk.slava@gmail.com>
2020-12-09 13:52:28 +0300

    edit comment
    Правка комментария window

--- Lyric/AppDelegate.swift
+++ Lyric/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         //Используеться для storyboard, если нет storyboard,
-        //можно использовать для собственного окна
+        //можно использовать для собственных нужд
         self.window = UIWindow(frame: UIScreen.main.bounds)
         
         let lyricViewController = LyricViewController.init(nibName: nil, bundle: nil)
