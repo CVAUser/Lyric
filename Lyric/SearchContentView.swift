c6b33ff18cd8d9505d024ed551c9661dc41d98d7
Slava <cherkasyuk.slava@gmail.com>
2020-12-13 12:40:03 +0300

    create search button

--- Lyric/SearchContentView.swift
+++ Lyric/SearchContentView.swift
@@ -10,26 +10,43 @@ import UIKit
 
 class SearchContentView: UIView {
     private let searchField = UITextField()
-    private let searchButton = UIButton()
+    private let searchButton = UIButton.init(type: UIButton.ButtonType.system)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor.lightGray
         addSubview(searchField)
+        addSubview(searchButton)
         customizeSearchField()
-        
+        customizeSeaarchButton()
         
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    //MARK: Customize search fields
+    
     func customizeSearchField() {
         searchField.translatesAutoresizingMaskIntoConstraints = false
         searchField.backgroundColor = UIColor.white
         searchField.placeholder = "Search artist or track"
         searchField.borderStyle = .roundedRect
         searchField.topAnchor.constraint(equalTo: topAnchor, constant:10).isActive = true
-        searchField.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        searchField.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        searchField.leftAnchor.constraint(equalTo: leftAnchor, constant:10).isActive = true
+        searchField.rightAnchor.constraint(equalTo: rightAnchor, constant:-10).isActive = true
     }
+    
+    func customizeSeaarchButton() {
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        searchButton.setTitle("Search", for: UIControl.State.normal)
+        searchButton.backgroundColor = UIColor.white
+        searchButton.tintColor = UIColor.black
+        searchButton.layer.cornerRadius = 5
+        searchButton.clipsToBounds = true
+        searchButton.topAnchor.constraint(equalTo: searchField.bottomAnchor, constant: 20).isActive = true
+        searchButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 10).isActive = true
+        searchButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -10).isActive = true
+    }
+    
 }
